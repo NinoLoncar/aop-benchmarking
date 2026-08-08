f263ea00244751a55e354bf846e5af2f74dd2c5e
NinoLoncar <nino.loncar16@gmail.com>
2026-08-08 13:34:44 +0200

    change baseline advice

--- src/main/java/com/foi/nloncar/baseline/NoOpAspect.java
+++ src/main/java/com/foi/nloncar/baseline/NoOpAspect.java
@@ -1,14 +1,12 @@
 package com.foi.nloncar.baseline;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class NoOpAspect {
 
-	@Around("execution(* com.foi.nloncar.baseline.NoOpTarget.advisedNoOp())")
-	public Object around(ProceedingJoinPoint pjp) throws Throwable {
-		return pjp.proceed();
+	@After("execution(* com.foi.nloncar.baseline.NoOpTarget.advisedNoOp())")
+	public void after() {
 	}
 }
